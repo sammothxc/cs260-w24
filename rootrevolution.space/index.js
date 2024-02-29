0d4d60abb6b56b76b85332025325b92f52c92354
sammothxc <sdwarrior8@gmail.com>
2024-02-28 22:23:03 -0700

    update

--- rootrevolution.space/index.js
+++ rootrevolution.space/index.js
@@ -2,21 +2,29 @@ document.addEventListener("DOMContentLoaded", function() {
     // Fetch username from local storage
     let username = localStorage.getItem("username");
     const loginStatusElement = document.getElementById("loginStatus");
-    
+    const accountLinkElement = document.getElementById("accountLink");
     if (username) {
         // User is logged in, display username and logout button
-        const logoutButton = document.createElement("button");
-        logoutButton.textContent = "Logout";
-        logoutButton.onclick = function() {
+        const logoutLink = document.createElement("a");
+        logoutLink.textContent = "Logout";
+        logoutLink.href = "#";
+        logoutLink.onclick = function() {
             // Remove username from local storage
             localStorage.removeItem("username");
             // Remove the welcome message flag from local storage
             localStorage.removeItem("welcomeMessageDisplayed");
             // Reload the page to reflect changes
             location.reload();
+
+            return false;
         };
-        loginStatusElement.textContent = username;
-        loginStatusElement.appendChild(logoutButton);
+        logoutLink.classList.add("li");
+        loginStatusElement.appendChild(logoutLink);
+        const userAccount = document.createElement("a");
+        userAccount.textContent = username;
+        userAccount.href = "account.html";
+        userAccount.classList.add("li");
+        accountLinkElement.appendChild(userAccount);
     } else {
         // User is not logged in, display login link
         const loginLink = document.createElement("a");
