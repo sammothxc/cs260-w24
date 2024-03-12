1bbb71521d492e8e05702523f91304139ffd8051
sammothxc <sdwarrior8@gmail.com>
2024-03-11 18:28:38 -0600

    update

---
+++ rootrevolution.space/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+// The service port. In production the front-end code is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+  });
+  
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
