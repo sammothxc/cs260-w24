4a538065f52a0b96c096f988b1e222c33f52d488
sammothxc <sdwarrior8@gmail.com>
2024-03-11 20:34:14 -0600

    update

--- rootrevolution.space/public/js/about.js
+++ rootrevolution.space/public/js/about.js
@@ -53,3 +53,23 @@ function updateSeedCounter() {
     localStorage.setItem("seedCounter", currentCount);
     seedCounterElement.textContent = currentCount + " Seeds Donated Since 2024!";
 }
+
+function displayQuote(data) {
+    fetch('https://api.quotable.io/random')
+      .then((response) => response.json())
+      .then((data) => {
+        const containerEl = document.querySelector('#carbon-footprint');
+  
+        const quoteEl = document.createElement('p');
+        quoteEl.classList.add('quote');
+        const authorEl = document.createElement('p');
+        authorEl.classList.add('author');
+  
+        quoteEl.textContent = data.content;
+        authorEl.textContent = data.author;
+  
+        containerEl.appendChild(quoteEl);
+        containerEl.appendChild(authorEl);
+      });
+  }
+  
