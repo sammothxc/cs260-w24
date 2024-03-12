4778827fa2b66bc4825c1575329ee422a2328e9d
sammothxc <sdwarrior8@gmail.com>
2024-03-11 19:38:56 -0600

    update

--- rootrevolution.space/index.js
+++ rootrevolution.space/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 
-// The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 app.use(express.json());
