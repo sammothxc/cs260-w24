d4c9f4f0f4538cc0804aece638fc84e231eb031a
sammothxc <sdwarrior8@gmail.com>
2024-04-13 03:43:10 -0600

    update

---
+++ public/js/chat.js
@@ -0,0 +1,16 @@
+var socket = io();
+var form = $('#form');
+var input = $('#input');
+
+form.submit(function (e) {
+    e.preventDefault(); // prevents page reloading
+    var messageInput = input.val();
+    var username = localStorage.getItem('username'); // Retrieve username from localStorage
+    socket.emit('chat message', { message: messageInput, username: username }); // Send message and username
+    input.val('');
+    return false;
+});
+
+socket.on('chat message', function (msg) {
+    $('#messages').append($('<li>').text(msg.username + ': ' + msg.message)); // Display username along with the message
+});
