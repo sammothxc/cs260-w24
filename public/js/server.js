b676331ed63b79443eeefa711b1f9a76bea9876a
sammothxc <sdwarrior8@gmail.com>
2024-04-13 03:10:12 -0600

    update

---
+++ public/js/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+    console.log('Client connected to WebSocket server');
+
+    ws.on('message', (message) => {
+        console.log('Received message:', message);
+        // Broadcast message to all connected clients
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+});
+
+server.listen(3000, () => {
+    console.log('WebSocket server is running on port 3000');
+});
