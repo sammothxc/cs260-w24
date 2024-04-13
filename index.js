ea602d5c855ce8fd5f90e9662e14d477bcaec10b
sammothxc <sdwarrior8@gmail.com>
2024-04-13 01:42:18 -0600

    update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
 const DB = require('./database.js');
+const { peerProxy } = require('./peerProxy.js');
 
 const authCookieName = 'token';
 
